d81c06c48993c061e17b6f678e9d22922f3169ac
LanluZ <LanluZ@qq.com>
2024-04-13 17:01:26 +0800

    修复 负数bug

--- Q1.5/predict.py
+++ Q1.5/predict.py
@@ -21,7 +21,7 @@ def predict(model_path, origin_x, predict_seq_time, scaler_x, scaler_y):
         # 新行准备
         new_line_x = copy.copy(origin_x[0])
         new_line_x[0] += origin_x.shape[0]  # 日期更改
-        new_line_x[1] = result[-1]  # 货值更改
+        new_line_x[1] = result[-1] if result[-1] > 0 else 0  # 货值更改
 
         # 旧行删除
         origin_x = np.delete(origin_x, 0, axis=0)
